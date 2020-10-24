6c44985afa43ca35f2a919371d12307ea385c9f2
J3D118076 <71446749+J3D118076@users.noreply.github.com>
2020-10-24 20:18:07 +0700

    Update scripts.js
    035

--- 035/assets/js/scripts.js
+++ 035/assets/js/scripts.js
@@ -49,6 +49,26 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
+
+    //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
